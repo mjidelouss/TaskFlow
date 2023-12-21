16fd0e12bb1af2b71e11518670edfaeb0419206b
mjidelouss <mjid.elouss@gmail.com>
2023-12-21 14:49:30 +0100

    Added Enums

--- src/main/java/com/example/taskflow/entities/User.java
+++ src/main/java/com/example/taskflow/entities/User.java
@@ -1,4 +1,21 @@
 package com.example.taskflow.entities;
 
+import com.example.taskflow.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 }
---
+++ src/main/java/com/example/taskflow/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.taskflow.enums;
+
+public enum UserRole {
+
+}
