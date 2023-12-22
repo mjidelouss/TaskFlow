058469cfd6b8b4a519804963a0bd0f4b7b5a98ca
mjidelouss <mjid.elouss@gmail.com>
2023-12-22 11:48:57 +0100

    Added Authentication Controller

---
+++ src/main/java/com/example/taskflow/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.example.taskflow.controller;
+
+import com.example.taskflow.Dtos.request.SignInRequest;
+import com.example.taskflow.Dtos.request.SignUpRequest;
+import com.example.taskflow.Dtos.response.JwtAuthenticationResponse;
+import com.example.taskflow.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
--- src/main/java/com/example/taskflow/controller/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskflow.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-
-}
