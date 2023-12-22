aae28e684ffd9fc951ab9cef35ebd969da7556ba
mjidelouss <mjid.elouss@gmail.com>
2023-12-22 11:44:56 +0100

    Added AuthenticationService and its Implementation

--- src/main/java/com/example/taskflow/service/Impl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/taskflow/service/Impl/AuthenticationServiceImpl.java
@@ -9,7 +9,6 @@ import com.example.taskflow.repository.UserRepository;
 import com.example.taskflow.service.AuthenticationService;
 import com.example.taskflow.service.JwtService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
