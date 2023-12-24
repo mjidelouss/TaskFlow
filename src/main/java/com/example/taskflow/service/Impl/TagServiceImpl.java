78462c33c92998654b2d7c933e856291bb861b31
mjidelouss <mjid.elouss@gmail.com>
2023-12-24 19:35:11 +0100

    Added The CRUD methodes

--- src/main/java/com/example/taskflow/service/Impl/TagServiceImpl.java
+++ src/main/java/com/example/taskflow/service/Impl/TagServiceImpl.java
@@ -1,9 +1,42 @@
 package com.example.taskflow.service.Impl;
 
+import com.example.taskflow.entities.Tag;
+import com.example.taskflow.repository.TagRepository;
 import com.example.taskflow.service.TagService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class TagServiceImpl implements TagService {
 
+    private final TagRepository tagRepository;
+
+    @Override
+    public List<Tag> getTags() {
+        return tagRepository.findAll();
+    }
+
+    @Override
+    public Tag getTagById(Long id) {
+        return tagRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Tag createTag(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    @Override
+    public Tag updateTag(Tag tag, Long id) {
+        Tag existingTag = getTagById(id);
+        return tagRepository.save(tag);
+    }
+
+    @Override
+    public void deleteTag(Long id) {
+        tagRepository.deleteById(id);
+    }
 }
