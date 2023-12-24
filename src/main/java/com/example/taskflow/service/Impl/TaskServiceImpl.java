582bdaaae4d50c432085b67d713b05126ab7393b
mjidelouss <mjid.elouss@gmail.com>
2023-12-24 18:54:34 +0100

    Added TaskServiceImpl methodes for CRUD

--- src/main/java/com/example/taskflow/service/Impl/TaskServiceImpl.java
+++ src/main/java/com/example/taskflow/service/Impl/TaskServiceImpl.java
@@ -1,9 +1,42 @@
 package com.example.taskflow.service.Impl;
 
+import com.example.taskflow.entities.Task;
+import com.example.taskflow.repository.TaskRepository;
 import com.example.taskflow.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
+    private final TaskRepository taskRepository;
+
+    @Override
+    public List<Task> getTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public Task updateTask(Task task, Long id) {
+        Task existingTask = getTaskById(id);
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
